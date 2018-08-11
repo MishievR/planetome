7534d91fcc1d9e7da307477e28d2f627eb1aabee
Planetome Web App <roman@plametome.com>
2018-08-12 00:11:36 +0200

    Done Pre-launch

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -21,27 +21,27 @@ class JobsController < ApplicationController
     def create
       @job = Job.new(job_params)
 
-      @amount = 900
-
-      # token = params[:stripeToken]
-      # email = params[:email_to_contact]
-
-      customer = Stripe::Customer.create(
-        :email => params[:email_to_contact],
-        :source  => params[:stripeToken]
-      )
-
-      charge = Stripe::Charge.create(
-        :customer    => customer.id,
-
-        # :source => token,
-        :amount      => @amount,
-        :currency    => 'usd',
-        :description => @job.position,
-        :statement_descriptor => @job.position,
-        # :email => email,
-
-      )
+      # @amount = 900
+      #
+      # # token = params[:stripeToken]
+      # # email = params[:email_to_contact]
+      #
+      # customer = Stripe::Customer.create(
+      #   :email => params[:email_to_contact],
+      #   :source  => params[:stripeToken]
+      # )
+      #
+      # charge = Stripe::Charge.create(
+      #   :customer    => customer.id,
+      #
+      #   # :source => token,
+      #   :amount      => @amount,
+      #   :currency    => 'usd',
+      #   :description => @job.position,
+      #   :statement_descriptor => @job.position,
+      #   # :email => email,
+      #
+      # )
       if @job.save
         flash[:success] = "#{@job.position} position at #{@job.employer.company_name} was created succesfully"
         redirect_to @job
@@ -49,9 +49,9 @@ class JobsController < ApplicationController
         render 'new'
       end
 
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to new_job_path
+    # rescue Stripe::CardError => e
+    #   flash[:error] = e.message
+    #   redirect_to new_job_path
 
     end
 
